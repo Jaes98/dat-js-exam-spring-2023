120bb9af34d386c9863ecd7c73b44546bbc53198
Jaes98 <marcus.jaes@gmail.com>
2023-08-22 12:40:02 +0200

    eksamen resultat

--- environment-01/script.js
+++ environment-01/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+let studentList = [];
+
+async function initApp() {
+  studentList = await getData();
+  console.log(studentList);
+  filterStudentsByEnrolled();
+  showStudents();
+}
+
+async function getData() {
+  const response = await fetch("students.json");
+  const data = await response.json();
+
+  return data;
+}
+
+function showStudents() {
+  document.querySelector("#students-list").innerHTML = "";
+
+  for (const student of studentList) {
+    showStudent(student);
+  }
+}
+
+function showStudent(student) {
+  const myHTML = `
+    <li>${student.name} - ${student.mail}, semester: ${student.semester}, enrolled: ${student.enrolled} </li>
+    `;
+
+  document
+    .querySelector("#students-list")
+    .insertAdjacentHTML("beforeend", myHTML);
+}
+
+function filterStudentsByEnrolled(params) {
+  studentList = studentList.filter((student) => student.enrolled);
+}
